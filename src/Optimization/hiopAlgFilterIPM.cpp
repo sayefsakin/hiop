08d145b95ba3efbb4381d4ce48ae05d4849b71e0
nychiang <sorakid507@gmail.com>
2022-06-10 20:13:47 -0700

    fix randomized alg

--- src/Optimization/hiopAlgFilterIPM.cpp
+++ src/Optimization/hiopAlgFilterIPM.cpp
@@ -1667,7 +1667,7 @@ hiopSolveStatus hiopAlgFilterIPMNewton::run()
   hiopKKTLinSys* kkt = decideAndCreateLinearSystem(nlp);
   assert(kkt != NULL);
   
-  if(nlp->options->GetString("reg_method")=="dual_first" && nlp->options->GetString("KKTLinsys")=="normaleqn") {
+  if(nlp->options->GetString("reg_priority")=="dual_first" && nlp->options->GetString("KKTLinsys")=="normaleqn") {
     pd_perturb_ = new hiopPDPerturbationDualFirst();
   } else {
     pd_perturb_ = new hiopPDPerturbation();
